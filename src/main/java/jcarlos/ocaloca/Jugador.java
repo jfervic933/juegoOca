afde7d40e9d448b6fd311ad688b202ac10241d45
JCarlosVico <jfervic933@maralboran.es>
2021-03-02 18:32:08 +0100

    Turno de un jugador

--- src/main/java/jcarlos/ocaloca/Jugador.java
+++ src/main/java/jcarlos/ocaloca/Jugador.java
@@ -23,20 +23,55 @@ public class Jugador {
     public void tirarDado() {
         this.tirada = r.nextInt(6) + 1;
     }
-    
+
     // Mira la tirada y mueve a la nueva casilla. Controla el rebote   
     public void mover(int movimiento){
         this.casillaActual+=movimiento;
         // Hay que controlar que no se salga del tablero
-        if (this.casillaActual>=Tablero.TOTAL_CASILLAS){
+        if (this.casillaActual >= Tablero.TOTAL_CASILLAS) {
             // Rebote
             int rebote = this.casillaActual - (Tablero.TOTAL_CASILLAS - 1);
-            this.casillaActual =  Tablero.TOTAL_CASILLAS - 1 - rebote;
+            this.casillaActual = Tablero.TOTAL_CASILLAS - 1 - rebote;
         }
     }
-    
-    public boolean ganaPartida(){
+
+    public boolean ganaPartida() {
         return this.casillaActual == Tablero.TOTAL_CASILLAS - 1;
+    }
+
+    public void jugarTurno(Tablero tablero) {
+        TipoCasilla tipoCasilla;
+        // Si el jugador puede jugar su turno
+        if (this.turnosSinJugar == 0) {
+            do {
+                // Tira dados
+                this.tirarDado();
+                Vista.informarTirada(this);
+                // Mueve al jugador
+                this.mover(this.tirada);
+                // Informa del movimiento
+                Vista.informarProgreso(this);
+                // Miro la casilla en la que ha caido
+                tipoCasilla = tablero.getCasilla(this.getCasillaActual()).getTipo();
+                // Actualiza la casilla actual del jugador
+                if (tipoCasilla.getSiguienteMovimiento() > 0) {
+                    // Se mueve de nuevo
+                    this.mover(tipoCasilla.getSiguienteMovimiento());
+                    // Informa del movimiento
+                    Vista.informarProgreso(this);
+                    // Mira la nueva casilla
+                    tipoCasilla = tablero.getCasilla(this.casillaActual).getTipo();
+                }
+                // Actualizo al jugador según la información de esa casilla
+                this.tiraOtraVez = tipoCasilla.isTiradaExtra();
+                this.turnosSinJugar = tipoCasilla.getTurnosSinJugar();
+            } while (this.tiraOtraVez);
+        } else { // No juega esa ronda
+            // El pozo tiene 100 turnos sin jugar, es imposible que en 
+            // 100 turnos no haya sobrepasado algún jugador el pozo
+            this.turnosSinJugar -= 1;
+            Vista.informarPierdeTurno(this);
+        }
     }
 
     @Override
