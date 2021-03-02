b043d0395999e5f9f1de02448cb9b8c96979c3f9
JCarlosVico <jfervic933@maralboran.es>
2021-03-02 18:30:45 +0100

    Mensajes clase Vista

--- src/main/java/jcarlos/ocaloca/Vista.java
+++ src/main/java/jcarlos/ocaloca/Vista.java
@@ -5,6 +5,7 @@ package jcarlos.ocaloca;
 public class Vista {
     
     public static void mostrarTablero(Tablero t){
+        System.out.println("");
         System.out.println(t.toString());
     }
     
@@ -14,5 +15,10 @@ public class Vista {
     
     public static void informarProgreso(Jugador j){
         System.out.println(j.getNombre() + " se mueve a la casilla " + j.getCasillaActual());
+    }
+    
+    public static void informarPierdeTurno(Jugador j){
+        System.out.println("El jugador " + j.getNombre() + 
+                " pierde su turno. Le quedan " + j.getTurnosSinJugar() + " turnos sin jugar");
     }
 }
